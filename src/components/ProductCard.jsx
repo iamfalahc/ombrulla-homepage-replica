ac168f07f19ec533ac2d31b530d20ad4638361cc
Muhammed Falah <iamfalahc@gmail.com>
2024-05-08 23:46:17 +0530

    Products section completed

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import LinkButton from './LinkButton'
 
 const ProductCard = ({data}) => {
   return (
-    <div>
-      <h2 className='xl:'>{data.title}</h2>
-      <span>{data.description}</span>
-      <img src={data.image} alt="" />
-    </div>
+    <div className='flex flex-col gap-y-8 bg-slate-100 xl:px-10 xl:py-10 rounded-2xl px-6 py-6 '>
+    <h2 className='xl:text-2xl font-normal'>{data.title}</h2>
+    <span className='text-gray-500'>{data.description}</span>
+    <img src={data.image} alt="" className='w-32 mx-auto ' />
+    <LinkButton/>
+  </div>
+  
   )
 }
 
--- src/containers/Products.jsx
+++ src/containers/Products.jsx
@@ -5,13 +5,13 @@ import ProductCard from "../components/ProductCard";
 const Products = () => {
   return (
 
-<div className="xl:px-24 xl:w-1/3 xl:flex xl:justify-between xl:w-full xl:gap-20">
-      <section className="xl:w-1/3">
+<div className="xl:px-24 xl:w-1/3 xl:flex xl:justify-between xl:w-full xl:gap-10 mb-14 px-4 py-2 ">
+      <section className="xl:w-1/3 mb-10">
         <Title title={"PRODUCTS"} />
-        <h2 className="xl:text-5xl xl:font-semibold xl:leading-tight">
+        <h2 className="xl:text-5xl xl:font-semibold xl:leading-tight text-2xl font-semibold leading-loose">
           Tracking Tools
         </h2>
-        <span>
+        <span className=" text-gray-500 leading-snug">
           AI and IoT-enabled tracking tools enhance workplace safety by
           providing real-time monitoring of workers' movements and vital signs.
           Simultaneously, they contribute to environmental sustainability by
@@ -19,9 +19,9 @@ const Products = () => {
           resource efficiency.
         </span>
       </section>
-      <section className="xl:flex xl:w-2/3 xl:gap-20">
+      <section className="xl:flex xl:flex-row xl:w-2/3 xl:gap-10 flex flex-col gap-10">
         {productData.map((data)=>{
-           return <div><ProductCard data={data}/></div>
+           return <ProductCard data={data}/>
         })}
       </section>
   
